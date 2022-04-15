13acfae35c4091b06d562663cd52221619cf891a
Philippe Willot <willot.ph@gmail.com>
2022-04-15 19:27:44 +0200

    Adding last task advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,9 @@
 # Make change to ssh configuration file to connect to our web server
+
+exec {'sudo sed -i "s/^.*PasswordAuthentication.*$/    PasswordAuthentication no/g" /etc/ssh/ssh_config':
+  path => ['/usr/bin']
+}
+
+exec {'sudo sed -i "s/^.*id_rsa.*$/    IdentityFile ~\/.ssh\/school/g" /etc/ssh/ssh_config':
+  path => ['/usr/bin']
+}
