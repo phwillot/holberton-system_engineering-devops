2fd403ef44b10823b29687fe9367951e3a31282c
Philippe Willot <willot.ph@gmail.com>
2022-04-20 16:54:03 +0200

    Installing puppet on server and modyfing the manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,6 +11,6 @@ exec {'echo "Hellow World" > /var/www/html/index.nginx-debian.html':
   path    => ['/usr/bin'],
 }
 
-exec {"sed -i '/listen \[::\]:80 default_server;/a\\trewrite ^\/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' /etc/nginx/sites-available/default":
+exec {"sed -i '/listen \[::\]:80 default_server;/a\        rewrite ^\/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default":
   path    => ['/usr/bin'],
 }
