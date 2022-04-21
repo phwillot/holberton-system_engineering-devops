7f090a9f48d28f83c2d427bdf9a1fcec232b2aab
Philippe Willot <willot.ph@gmail.com>
2022-04-21 12:03:28 +0200

    removing entire folder error on repo

--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD' "$1"
