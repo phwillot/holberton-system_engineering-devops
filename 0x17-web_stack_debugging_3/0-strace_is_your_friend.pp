f2d351ba1e60dc24bb4222b3e6c8136495fb7317
Philippe Willot <willot.ph@gmail.com>
2022-06-01 15:33:50 +0200

    fix bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # this manifest replace the mistake in php file
-exec {'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php':
+exec {'sed -i "s/phpp/php/" /var/www/html/wp-settings.php':
   path    =>  ['/bin']
 }
