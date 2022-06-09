a09466b78443b7650b3da3f86c86ced0acbdfa0a
Philippe Willot <willot.ph@gmail.com>
2022-06-09 12:28:47 +0200

    fix task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # this manifest will fix the requests on the nginx server
-exec {'sed -i '$d' /etc/default/nginx && echo ULIMIT=\"-n 4096\" >> /etc/default/nginx && sudo service nginx restart':
-  path    =>  ['/bin']
+exec {"sed -i 's/n 15/n 4096/' /etc/default/nginx && sudo service nginx restart":
+  path    =>  ['/bin', '/usr/bin']
 }
